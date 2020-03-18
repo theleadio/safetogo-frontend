8e5563c65eb1d14c6be7650b4bb8cd2528f977b0
Edmund Hee <edmund.hee05@gmail.com>
2020-03-19 03:26:32 +0800

    HTML render for google login

--- components/SearchBar.vue
+++ components/SearchBar.vue
@@ -65,6 +65,7 @@ export default {
       state: 'idle',
       focusCounter: 0,
       focusTimeoutId: null,
+      userProfile: {}
     }
   },
   methods:{
@@ -119,7 +120,6 @@ export default {
   mounted: function (){
     this.$nextTick(()=>{
       console.log("nextTick")
-      let userProfile = {};
       gapi.load('auth2,signin2', function() {
         let auth2 = gapi.auth2.init({
           client_id: '468040312422-9jeej0dqrcjis4vt0k6rt7g2lg3tsaja.apps.googleusercontent.com',
