f4aaa02f300afc787b2973102ee8575fc6a89dc9
Edmund Hee <edmund.hee05@gmail.com>
2020-03-19 01:22:55 +0800

    Updated with google login

--- components/Login.vue
+++ components/Login.vue
@@ -16,13 +16,11 @@ export default {
     },
     methods:{
         onSignIn: function(googleUser){
-            function onSignIn(googleUser) {
-                let profile = googleUser.getBasicProfile();
-                console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-                console.log('Name: ' + profile.getName());
-                console.log('Image URL: ' + profile.getImageUrl());
-                console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-        }
+            let profile = googleUser.getBasicProfile();
+            console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+            console.log('Name: ' + profile.getName());
+            console.log('Image URL: ' + profile.getImageUrl());
+            console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
     }
 }
 </script>
