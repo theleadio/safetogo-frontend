49734a433d639f6ec9a9fcf3f48e4c43fb1ad845
Edmund Hee <edmund.hee05@gmail.com>
2020-03-19 01:14:15 +0800

    Updated meta for google login

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,7 @@ module.exports = {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
-      { name: 'google-signin-client_id', content:'468040312422-scubvil1bm9n2n72trikhnadc99a1kue.apps.googleusercontent.com'} //ZSzf2Icc5kV7SXxHAzJ-5ZId
+      { name: 'google-signin-client_id', content:'468040312422-9jeej0dqrcjis4vt0k6rt7g2lg3tsaja.apps.googleusercontent.com'} //WqaH-4rPN-FRaEhR6FJF9uSB
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
@@ -21,7 +21,6 @@ module.exports = {
       { src: 'https://code.jquery.com/jquery-3.4.1.slim.min.js', type: 'text/javascript' },
       { src: 'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js', type: 'text/javascript' },
       { src: 'https://kit.fontawesome.com/48d20e9c42.js', crossorigin: 'anonymous', type:'text/javascript'},
-      { src:"https://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v6.0&appId=1555723304582235&autoLogAppEvents=1", crossorigin:"anonymous"},
       { src: "https://apis.google.com/js/platform.js"}
     ]
   },
