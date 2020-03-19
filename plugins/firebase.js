ea285be1644f8172f0fd658aefc8fbcdcbb56263
Edmund Hee <edmund.hee05@gmail.com>
2020-03-20 01:43:12 +0800

    HTML render for google login

---
+++ plugins/firebase.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase'
+
+let firebaseConfig = {
+    apiKey: "AIzaSyBknSNrG_yR7FLMVJ01svDWlYte6GFvoL0",
+    authDomain: "safetogo-271515.firebaseapp.com",
+    databaseURL: "https://safetogo-271515.firebaseio.com",
+    projectId: "safetogo-271515",
+    storageBucket: "safetogo-271515.appspot.com",
+    messagingSenderId: "468040312422",
+    appId: "1:468040312422:web:4bba6be9a62e2a4442afa7"
+  };
+  // Initialize Firebase
+let app = null
+if (!firebase.apps.length) {
+    app = firebase.initializeApp(firebaseConfig)
+}else{
+    app = firebase.app()
+}
+
+export const db = app.database()
