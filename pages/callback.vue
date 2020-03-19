43352ad0376ab7deb0dcd805c34be86d7f7ad4c5
Edmund Hee <edmund.hee05@gmail.com>
2020-03-20 00:15:40 +0800

    HTML render for google login

---
+++ pages/callback.vue
@@ -0,0 +1,13 @@
+  
+<template>
+    <div class="container">
+    <img src="~/assets/img/loading.svg" alt="Loading..." width="80px" />
+    Logging in...
+    </div>
+</template>
+
+<style scoped>
+.container {
+  min-height: 70vh;
+}
+</style>
