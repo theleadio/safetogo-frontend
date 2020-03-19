91ecc782e54b2b9f1ae1572782926d404dce8bc6
Edmund Hee <edmund.hee05@gmail.com>
2020-03-20 01:57:07 +0800

    HTML render for google login

--- components/SearchBar.vue
+++ components/SearchBar.vue
@@ -49,7 +49,8 @@
 <script>
 
 import { mapMutations, mapGetters } from 'vuex'
-import firebase from 'firebase'
+import firebase from 'firebase/app'
+import 'firebase/auth'
 
 export default {
   name: 'Search',
