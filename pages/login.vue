72428f3f49b4dfdcfa7d4b6244fd8fff5c59d6e9
Edmund Hee <edmund.hee05@gmail.com>
2020-03-19 23:42:53 +0800

    HTML render for google login

---
+++ pages/login.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <p>{{$auth.$state.redirect}}</p>
+        <p>You have to login before accessing to <strong>{{ $auth.$state.redirect }}</strong></p>
+        <p>{{fetchUser}}</p>
+    </div>
+</template>
+<script>
+export default {
+    middleware:['auth'],
+    computed:{
+        fetchUser(){
+            return JSON.stringify(this.$auth.fetchUser(), undefined, 2)
+        }
+    }
+}
+</script>
