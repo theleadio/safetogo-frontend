3788b81ff8841cc383778f855c816ee330cd33c2
Edmund Hee <edmund.hee05@gmail.com>
2020-04-08 20:12:43 +0800

    Filter visualise donw

--- store/map.js
+++ store/map.js
@@ -8,6 +8,13 @@ export const state = () => defaultState();
 
 export const mutations = {
     loadSummary(state, summary){
+        // for (let i in summary){
+        //     state.summaryMarkers.push({
+                
+        //     });
+        // }
+
+        console.log(sumary)
         state.summaryMarkers = summary
     }
 }
